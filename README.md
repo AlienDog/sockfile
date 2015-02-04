6bee0ea26dbdef0a9846875ae4a7edae83ce3f30
Steven Smith <Steveice10@gmail.com>
2015-02-03 17:54:44 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+<b><center><h1>sockfile</h></center></b>
+==========
+
+Sends files prefixed with their size over the network.
---
+++ source/main.cpp
@@ -0,0 +1,68 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <string>
+
+int main(int argc, const char* argv[]) {
+	if(argc != 3) {
+		printf("Usage: %s ip file\n", argv[0]);
+		return -1;
+	}
+
+	FILE* fd = fopen(argv[2], "r");
+	if(!fd) {
+		printf("Failed to open file: %s\n", strerror(errno));
+		return -1;
+	}
+
+	fseek(fd, 0, SEEK_END);
+	uint64_t size = (uint64_t) ftell(fd);
+	fseek(fd, 0, SEEK_SET);
+
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0) {
+		printf("Failed to open socket: %s\n", strerror(errno));
+		return -1;
+	}
+
+	struct sockaddr_in address;
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_port = htons(5000);
+	inet_aton(argv[1], &address.sin_addr);
+
+	if(connect(sock, (struct sockaddr *) &address, sizeof(address)) < 0) {
+		printf("Failed to connect: %s\n", strerror(errno));
+		return -1;
+	}
+
+	printf("Sending info...\n");
+	fflush(stdout);
+
+	uint64_t fileSize = size;
+	static const int num = 42;
+	if(*((char*) &num) == num) {
+		fileSize = (((uint64_t) htonl((uint32_t) fileSize)) << 32) + htonl((uint32_t) (fileSize >> 32));
+	}
+
+	write(sock, &fileSize, sizeof(fileSize));
+
+	printf("Sending file...\n");
+	fflush(stdout);
+
+	uint64_t bufSize = 1024 * 16; // 16KB
+	void* buf = malloc(bufSize);
+	for(uint64_t pos = 0; pos < size; pos += bufSize) {
+		size_t read = fread(buf, 1, bufSize, fd);
+		write(sock, buf, read);
+	}
+
+	close(sock);
+	fclose(fd);
+
+	printf("File successfully sent.\n");
+
+	return 0;
+}
