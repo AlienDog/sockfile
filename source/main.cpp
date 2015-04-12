bb9eee57300e3444e354ec3b4b7bda8e0d9955c7
Steven Smith <Steveice10@gmail.com>
2015-04-12 14:26:43 -0700

    Spaces, not tabs, wait for server to finish receiving before closing.

--- source/main.cpp
+++ source/main.cpp
@@ -12,81 +12,90 @@
 #include <string>
 
 int main(int argc, const char* argv[]) {
-	if(argc != 3) {
-		printf("Usage: %s ip file\n", argv[0]);
-		return -1;
-	}
+    if(argc != 3) {
+        printf("Usage: %s ip file\n", argv[0]);
+        return -1;
+    }
 
 #ifdef __WIN32__
-	WORD versionWanted = MAKEWORD(1, 1);
-	WSADATA wsaData;
-	WSAStartup(versionWanted, &wsaData);
+    WORD versionWanted = MAKEWORD(1, 1);
+    WSADATA wsaData;
+    WSAStartup(versionWanted, &wsaData);
 #endif
 
-	FILE* fd = fopen(argv[2], "r");
-	if(!fd) {
-		printf("Failed to open file: %s\n", strerror(errno));
+    FILE* fd = fopen(argv[2], "r");
+    if(!fd) {
+        printf("Failed to open file: %s\n", strerror(errno));
 #ifdef __WIN32__
-		WSACleanup();
+        WSACleanup();
 #endif
-		return -1;
-	}
+        return -1;
+    }
 
-	fseek(fd, 0, SEEK_END);
-	uint64_t size = (uint64_t) ftell(fd);
-	fseek(fd, 0, SEEK_SET);
+    fseek(fd, 0, SEEK_END);
+    uint64_t size = (uint64_t) ftell(fd);
+    fseek(fd, 0, SEEK_SET);
 
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock < 0) {
-		printf("Failed to open socket: %s\n", strerror(errno));
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0) {
+        printf("Failed to open socket: %s\n", strerror(errno));
 #ifdef __WIN32__
-		WSACleanup();
+        WSACleanup();
 #endif
-		return -1;
-	}
+        return -1;
+    }
 
-	struct sockaddr_in address;
-	memset(&address, 0, sizeof(address));
-	address.sin_family = AF_INET;
-	address.sin_port = htons(5000);
-	address.sin_addr.s_addr = inet_addr(argv[1]);
-	if(connect(sock, (struct sockaddr *) &address, sizeof(address)) < 0) {
-		printf("Failed to connect: %s\n", strerror(errno));
+    struct sockaddr_in address;
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(5000);
+    address.sin_addr.s_addr = inet_addr(argv[1]);
+    if(connect(sock, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        printf("Failed to connect: %s\n", strerror(errno));
 #ifdef __WIN32__
-		WSACleanup();
+        WSACleanup();
 #endif
-		return -1;
-	}
+        return -1;
+    }
 
-	printf("Sending info...\n");
-	fflush(stdout);
+    printf("Sending info...\n");
+    fflush(stdout);
 
-	uint64_t fileSize = size;
-	static const int num = 42;
-	if(*((char*) &num) == num) {
-		fileSize = (((uint64_t) htonl((uint32_t) fileSize)) << 32) + htonl((uint32_t) (fileSize >> 32));
-	}
+    uint64_t fileSize = size;
+    static const int num = 42;
+    if(*((char*) &num) == num) {
+        fileSize = (((uint64_t) htonl((uint32_t) fileSize)) << 32) + htonl((uint32_t) (fileSize >> 32));
+    }
 
-	write(sock, &fileSize, sizeof(fileSize));
+    write(sock, &fileSize, sizeof(fileSize));
 
-	printf("Sending file...\n");
-	fflush(stdout);
+    printf("Sending file...\n");
+    fflush(stdout);
 
-	uint64_t bufSize = 1024 * 16; // 16KB
-	void* buf = malloc(bufSize);
-	for(uint64_t pos = 0; pos < size; pos += bufSize) {
-		size_t read = fread(buf, 1, bufSize, fd);
-		write(sock, buf, read);
-	}
+    uint64_t bufSize = 1024 * 16; // 16KB
+    void* buf = malloc(bufSize);
+    for(uint64_t pos = 0; pos < size; pos += bufSize) {
+        size_t read = fread(buf, 1, bufSize, fd);
+        write(sock, buf, read);
+    }
 
-	close(sock);
-	fclose(fd);
+    printf("Waiting for server to finish receiving...\n");
 
-	printf("File successfully sent.\n");
+    char temp;
+    while(read(sock, &temp, sizeof(temp)) > 0) {
+        sleep(1);
+    }
+
+    printf("Closing...\n");
+
+    close(sock);
+    fclose(fd);
+
+    printf("File successfully sent.\n");
 
 #ifdef __WIN32__
-	WSACleanup();
+    WSACleanup();
 #endif
 
-	return 0;
+    return 0;
 }
