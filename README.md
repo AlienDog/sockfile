0264ccbc9e519c229debdf5acc96fa31a1dd0b5b
Steven Smith <Steveice10@gmail.com>
2015-04-13 18:48:33 -0700

    Rewrite in Java to work across platforms more easily.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ==========
 
 Sends files prefixed with their size over the network.
+
+Build by running 'mvn clean install' in the project root directory.
+
+Run with 'java -jar sockfile.jar ip file'.
---
+++ src/main/java/org/spacehq/sockfile/Main.java
@@ -0,0 +1,67 @@
+package org.spacehq.sockfile;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Main {
+	public static void main(String args[]) {
+		if(args.length < 2) {
+			System.err.println("Usage: java -jar sockfile.jar <ip> <file>");
+			return;
+		}
+
+		File file = new File(args[1]);
+		if(!file.exists()) {
+			System.err.println("File \"" + args[1] + "\" does not exist.");
+			return;
+		}
+
+		Socket socket = null;
+		DataOutputStream out = null;
+		try {
+			socket = new Socket(args[0], 5000);
+			out = new DataOutputStream(socket.getOutputStream());
+		} catch(IOException e) {
+			System.err.println("Failed to open socket.");
+			e.printStackTrace();
+			return;
+		}
+
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(file);
+		} catch(IOException e) {
+			System.err.println("Failed to open file stream.");
+			e.printStackTrace();
+			return;
+		}
+
+		try {
+			System.out.println("Sending info...");
+			out.writeLong(file.length());
+
+			System.out.println("Sending file...");
+			byte buffer[] = new byte[1024 * 16];
+			int length = 0;
+			while((length = in.read(buffer)) != -1) {
+				out.write(buffer, 0, length);
+			}
+
+			System.out.println("File sent successfully.");
+		} catch(IOException e) {
+			System.err.println("Failed to send data.");
+			e.printStackTrace();
+			return;
+		} finally {
+			try {
+				in.close();
+				out.close();
+				socket.close();
+			} catch(IOException e) {
+			}
+		}
+	}
+}
