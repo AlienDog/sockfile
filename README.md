2b39b7187bf00d4b2c24ead534b285fa56e75abb
Steven Smith <Steveice10@gmail.com>
2015-07-20 11:02:57 -0700

    General cleanup, fixes, and re-versioning.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-<b><center>sockfile</center></b>
-==========
-
+# sockfile
 Sends files prefixed with their size over the network.
 
-Build by running 'mvn clean install' in the project root directory.
-
 Run with 'java -jar sockfile.jar ip file'.
+
+## Building the Source
+sockfile uses Maven to manage dependencies. Simply run 'mvn clean install' in the source's directory.
+
+Builds can be downloaded **[here](http://build.spacehq.org/job/sockfile)**.
+
+## License
+sockfile is licensed under the **[MIT license](http://www.opensource.org/licenses/mit-license.html)**.
--- src/main/java/org/spacehq/sockfile/Main.java
+++ src/main/java/org/spacehq/sockfile/Main.java
@@ -7,61 +7,61 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Main {
-	public static void main(String args[]) {
-		if(args.length < 2) {
-			System.err.println("Usage: java -jar sockfile.jar <ip> <file>");
-			return;
-		}
+    public static void main(String args[]) {
+        if(args.length < 2) {
+            System.err.println("Usage: java -jar sockfile.jar <ip> <file>");
+            return;
+        }
 
-		File file = new File(args[1]);
-		if(!file.exists()) {
-			System.err.println("File \"" + args[1] + "\" does not exist.");
-			return;
-		}
+        File file = new File(args[1]);
+        if(!file.exists()) {
+            System.err.println("File \"" + args[1] + "\" does not exist.");
+            return;
+        }
 
-		Socket socket = null;
-		DataOutputStream out = null;
-		try {
-			socket = new Socket(args[0], 5000);
-			out = new DataOutputStream(socket.getOutputStream());
-		} catch(IOException e) {
-			System.err.println("Failed to open socket.");
-			e.printStackTrace();
-			return;
-		}
+        Socket socket = null;
+        DataOutputStream out = null;
+        try {
+            socket = new Socket(args[0], 5000);
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch(IOException e) {
+            System.err.println("Failed to open socket.");
+            e.printStackTrace();
+            return;
+        }
 
-		FileInputStream in = null;
-		try {
-			in = new FileInputStream(file);
-		} catch(IOException e) {
-			System.err.println("Failed to open file stream.");
-			e.printStackTrace();
-			return;
-		}
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(file);
+        } catch(IOException e) {
+            System.err.println("Failed to open file stream.");
+            e.printStackTrace();
+            return;
+        }
 
-		try {
-			System.out.println("Sending info...");
-			out.writeLong(file.length());
+        try {
+            System.out.println("Sending info...");
+            out.writeLong(file.length());
 
-			System.out.println("Sending file...");
-			byte buffer[] = new byte[1024 * 128];
-			int length = 0;
-			while((length = in.read(buffer)) != -1) {
-				out.write(buffer, 0, length);
-			}
+            System.out.println("Sending file...");
+            byte buffer[] = new byte[1024 * 128];
+            int length = 0;
+            while((length = in.read(buffer)) != -1) {
+                out.write(buffer, 0, length);
+            }
 
-			System.out.println("File sent successfully.");
-		} catch(IOException e) {
-			System.err.println("Failed to send data.");
-			e.printStackTrace();
-			return;
-		} finally {
-			try {
-				in.close();
-				out.close();
-				socket.close();
-			} catch(IOException e) {
-			}
-		}
-	}
+            System.out.println("File sent successfully.");
+        } catch(IOException e) {
+            System.err.println("Failed to send data.");
+            e.printStackTrace();
+            return;
+        } finally {
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            } catch(IOException e) {
+            }
+        }
+    }
 }
